af00f45c6c6ee69b70c1bc7b0d7ed74383e4291d
Brett B <65505937+BreadBooks@users.noreply.github.com>
2021-01-17 12:31:28 -0600

    added pages & components folders

--- src/app.js
+++ src/app.js
@@ -1,1 +1,20 @@
-import { useState } from 'react';
+import "./App.css";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Dashboard from "./pages/dashboard"
+
+
+
+function App() {
+
+  return (
+    <Router >
+      <div className="App">
+        <Switch>
+          <Route path="/dashboard" exact component={Dashboard}><Dashboard /></Route>
+        </Switch>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
